6cfe7921a8a97c94d1b880dde939e6bdd58fcf44
Alvaro Segovia <alvarosego01@gmail.com>
2024-08-31 18:30:53 -0400

    Update wind instrument categories in profile interfaces

---
+++ src/modules/business/vacants/dto/index.ts
@@ -0,0 +1,6 @@
+
+
+export * from "./Update-vacant.dto";
+export * from "./Create-vacant.dto";
+export * from "./Create-postulation.dto";
+export * from "./Update-postulation.dto";
---
+++ src/modules/business/vacants/interfaces/index.ts
@@ -0,0 +1,4 @@
+
+
+export * from "./postulation";
+export * from "./vacants";
